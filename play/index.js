1de18b8ad53c4ddc8f8a75d042805e7c79cae767
Rodrigo Waltenberg <rodrigo@frametools.dev>
2020-04-27 04:42:16 -0300

    Fixed height scenario

--- play/index.js
+++ play/index.js
@@ -1,16 +1,17 @@
+import FixedHeight from './scenarios/FixedHeight.vue'
 import InfiniteScroller from '../src/InfiniteScroller.vue'
 import Vue from 'vue'
 import { play } from 'vue-play'
 
 Vue.component('infinite-scroller', InfiniteScroller)
 
-play('InfiniteScroller').add('Normal', {
-  template: `
+play('InfiniteScroller')
+  .add('Normal', {
+    template: `
     <infinite-scroller></infinite-scroller>
   `,
-  data() {
-    return {
-      count: 0
+    data() {
+      return {}
     }
-  }
-})
+  })
+  .add('Fixed height', h => h(FixedHeight))
